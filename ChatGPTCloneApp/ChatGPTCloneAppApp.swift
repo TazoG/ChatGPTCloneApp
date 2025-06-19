f956ebafc4f8dfb9d4d40d475df952ed90812329
Tazo <tazogigitashvili@gmail.com>
2025-06-19 20:15:28 +0400

    Initial commit chatgptcloneapp project

---
+++ ChatGPTCloneApp/ChatGPTCloneAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatGPTCloneAppApp.swift
+//  ChatGPTCloneApp
+//
+//  Created by Tazo Gigitashvili on 19.06.25.
+//
+
+import SwiftUI
+
+@main
+struct ChatGPTCloneAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
